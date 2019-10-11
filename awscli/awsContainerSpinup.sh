1ad2093c45dbeeb838cda090ac0598e8700b5c95
Raja Kumar Gupta <rajakumargupta.techie@gmail.com>
2019-10-12 01:43:32 +0530

    AWS CLI Dockerfile

---
+++ awscli/awsContainerSpinup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+# enable interruption signal handling
+trap - INT TERM
+
+docker run --rm \
+	-t $(tty &>/dev/null && echo "-i") \
+	-e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
+	-e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
+	-e "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" \
+	-v "$(pwd):/project" \
+        rajakumargupta/aws-cli:latest \
+        "$@"
